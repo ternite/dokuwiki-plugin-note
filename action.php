5c13bff4fef7142d7500a4f18c560f3eea44c9ad
Jonhen <jonhen@mail.ru>
2019-05-18 22:05:44 +0700

    Update action.php
    Does not work if base of the DokuWiki install has been moved.
    https://www.dokuwiki.org/devel:environment#doku

--- action.php
+++ action.php
@@ -24,33 +24,33 @@ class action_plugin_note extends DokuWiki_Action_Plugin {
         $event->data[] = array (
             'type' => 'picker',
             'title' => $this->getLang('note_picker'),
-            'icon' => '/lib/plugins/note/images/note_picker.png',
+            'icon' => DOKU_BASE.'lib/plugins/note/images/note_picker.png',
             'list' => array(
                 array(
                     'type'   => 'format',
                     'title'  => $this->getLang('tb_note'),
-                    'icon'   => '/lib/plugins/note/images/tb_note.png',
+                    'icon'   => DOKU_BASE.'lib/plugins/note/images/tb_note.png',
                     'open'   => '<note>',
                     'close'  => '</note>',
                 ),
                 array(
                     'type'   => 'format',
                     'title'  => $this->getLang('tb_tip'),
-                    'icon'   => '/lib/plugins/note/images/tb_tip.png',
+                    'icon'   => DOKU_BASE.'lib/plugins/note/images/tb_tip.png',
                     'open'   => '<note tip>',
                     'close'  => '</note>',
                 ),
                 array(
                     'type'   => 'format',
                     'title'  => $this->getLang('tb_important'),
-                    'icon'   => '/lib/plugins/note/images/tb_important.png',
+                    'icon'   => DOKU_BASE.'lib/plugins/note/images/tb_important.png',
                     'open'   => '<note important>',
                     'close'  => '</note>',
                 ),
                 array(
                     'type'   => 'format',
                     'title'  => $this->getLang('tb_warning'),
-                    'icon'   => '/lib/plugins/note/images/tb_warning.png',
+                    'icon'   => DOKU_BASE.'lib/plugins/note/images/tb_warning.png',
                     'open'   => '<note warning>',
                     'close'  => '</note>',
                 ),
